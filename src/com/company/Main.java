324f4734383789ad00b8ae2df8f00828afa3af93
IT-Kubat <kubat88@list.ru>
2019-11-02 23:25:53 +0600

    Homework 1.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	//
+        System.out.println("Привет мир");
+
+        String tx = "Привет";
+
+        System.out.println(tx);
+
+        String myWord = "Привет";
+
+        String myVoice = "Здрасти";
+
+
+        System.out.println("результат соединения:"+ "  " + myWord +"  " + myVoice );
+
+
+        final String tx2 = "Привет";
+        System.out.println(tx2);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
